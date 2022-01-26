e27d50fb4319d77a32f1bc6633e83d9333754fdb
mn-hs <mnich.hs@gmail.com>
2022-01-26 10:25:07 -0500

    Rewrote to post win/loss messages in scorebox.

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// gets player input
-function playerMove() {
-  const playerPlay = window.prompt('Choose: rock, paper, or scissors.');
-  let choice = playerPlay.toLowerCase();
-  
-  if (choice == 'rock'||choice == 'paper'||choice == 'scissors'){
-    return choice;
-  } else {
-    return 'Please choose a valid move';
-  }
-}
-
 // returns random move
 function computerMove(){
   const moves = ['rock', 'paper', 'scissors'];
@@ -17,76 +5,83 @@ function computerMove(){
   return(computerPlay, moves[computerPlay]);
 }
 
-// returns win/loss message 
-function playRound(playerSelect, computerSelect) {
-  if (playerSelect === 'rock' && computerSelect === 'rock') {
-    return 'Tie!';
+const moves = document.querySelectorAll('#rock, #paper, #scissors');
+moves.forEach((move) => {
+  move.addEventListener('click', () => {
+    if (move.id == 'rock'){
+      playRock();
+    }
+    else if (move.id == 'paper'){
+      playPaper();
+    }
+    else {
+      playScissors();
+    }
+  });
+});
+
+const scoreBox = document.querySelector('#scoreBox');
+
+function playRock(){
+  const inputRock = document.createElement('p');
+  inputRock.classList.add('inputRock')
+
+  if (computerMove() === 'rock') {
+    inputRock.textContent = 'Tie!';
   } 
-  else if (playerSelect === 'paper' && computerSelect === 'paper') {
-    return 'Tie!';
-  } 
-  else if (playerSelect === 'scissors' && computerSelect === 'scissors') {
-    return 'Tie!';
-  } 
-  else if (playerSelect === 'rock' && computerSelect === 'scissors') {
-    return `You win! rock beats scissors`;
-  } 
-  else if (playerSelect === 'paper' && computerSelect === 'rock') {
-    return `You win! paper beats rock`;
-  } 
-  else if (playerSelect === 'scissors' && computerSelect === 'paper') {
-    return `You win! scissors beats paper`;
-  } 
-  else if (playerSelect === 'rock' && computerSelect === 'paper') {
-    return `You lose! rock loses to paper`;
-  } 
-  else if (playerSelect === 'paper' && computerSelect === 'scissors') {
-    return `You lose! paper loses to scissors`;
-  } 
-  else if (playerSelect === 'scissors' && computerSelect === 'rock') {
-    return `You lose! scissors loses rock`;
+  else if (computerMove() === 'scissors') {
+    inputRock.textContent = `You win! rock beats scissors`;
+    playerScore++;
   } 
   else {
-    return `${playerSelect}`;
-  }
-} 
-
-/* function scoreGame() {
-  if (playRound == 'You win! rock beats scissors'||playRound == 'You win! scissors beats paper'
-  ||playRound == `You win! scissors beats paper`) {
-    playerScore++;
-  }
-  if (playRound == 'You lose! rock loses to paper'||playRound == 'You lose! scissors loses to rock'
-  ||playRound == `You lose! paper loses to scissors`) {
+    inputRock.textContent = `You lose! rock loses to paper`;
     computerScore++;
   }
-  } */
 
+  scoreBox.appendChild(inputRock)
+}
+
+function playPaper(){
+  const inputPaper = document.createElement('p');
+  inputPaper.classList.add('inputPaper')
+
+  if (computerMove() === 'paper') {
+    inputPaper.textContent = 'Tie!';
+  } 
+  else if (computerMove() === 'rock') {
+    inputPaper.textContent = `You win! paper beats rock`;
+    playerScore++;
+  } 
+  else {
+    inputPaper.textContent = `You lose! paper loses to scissors`;
+    computerScore++;
+  }
+
+  scoreBox.appendChild(inputPaper)
+}
+
+function playScissors(){
+  const inputScissors = document.createElement('p');
+  inputScissors.classList.add('inputScissors')
+
+  if (computerMove() === 'scissors') {
+    inputScissors.textContent = 'Tie!';
+  } 
+  else if (computerMove() === 'paper') {
+    inputScissors.textContent = `You win! scissors beats paper`;
+    playerScore++;
+  } 
+  else {
+    inputScissors.textContent = `You lose! scissors loses to rock`;
+    computerScore++;
+  }
+
+  scoreBox.appendChild(inputScissors)
+}
 
 let playerScore = 0;
 let computerScore = 0;
 
- function playGame() {
-   for (let i = 1; i <= 5; i++) {
-    alert(`ROUND ${i}`)
-    let round = playRound(playerMove(), computerMove());
-     alert(round);
 
-     if (round == 'You win! rock beats scissors' || round == 'You win! paper beats rock' || round == 'You win! scissors beats paper') {
-      playerScore++;
-    }
-    if (round == 'You lose! rock loses to paper' || round == 'You lose! paper loses to scissors' || round == 'You lose! scissors loses rock') {
-      computerScore++;
 
-    alert(`SCORE - You: ${playerScore}  -  Computer: ${computerScore}`);
-  }
-}
-  if (playerScore < computerScore) {
-    alert(`You lose :( ${computerScore} to ${playerScore}`)
-  }
-  else {
-    alert(`You win :) ${playerScore} to ${computerScore}`)
-  }
- }
-
- playGame();
+ 
